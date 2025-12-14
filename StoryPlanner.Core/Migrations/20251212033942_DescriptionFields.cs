e155929c74929eb38455936c880ef63631753698
Brian Cheng <bc414a@gmail.com>
2025-12-14 11:51:26 -0500

    Migrations

---
+++ StoryPlanner.Core/Migrations/20251212033942_DescriptionFields.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoryPlanner.Migrations
+{
+    /// <inheritdoc />
+    public partial class DescriptionFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ColorHex",
+                table: "Themes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Themes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Locations",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "CodexEntries",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "CodexEntries",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Archetype",
+                table: "Characters",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Characters",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Chapters",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ColorHex",
+                table: "Themes");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Themes");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "CodexEntries");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "CodexEntries");
+
+            migrationBuilder.DropColumn(
+                name: "Archetype",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Chapters");
+        }
+    }
+}
---
+++ StoryPlanner.Core/Migrations/20251213210257_AddingGeminiEntriesToModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoryPlanner.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingGeminiEntriesToModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GeminiEntries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Time = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Prompt = table.Column<string>(type: "TEXT", nullable: false),
+                    ResponseHtml = table.Column<string>(type: "TEXT", nullable: false),
+                    IsAnalyzed = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GeminiEntries", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GeminiEntries");
+        }
+    }
+}
