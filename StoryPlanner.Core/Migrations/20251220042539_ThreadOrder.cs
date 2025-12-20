6481698da92dc7e44d4166a31a4f9e3f05436978
Brian Cheng <bc414a@gmail.com>
2025-12-20 00:08:35 -0500

    migration

---
+++ StoryPlanner.Core/Migrations/20251220042539_ThreadOrder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoryPlanner.Migrations
+{
+    /// <inheritdoc />
+    public partial class ThreadOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SortOrder",
+                table: "PlotPointThreads",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SortOrder",
+                table: "PlotPointThreads");
+        }
+    }
+}
